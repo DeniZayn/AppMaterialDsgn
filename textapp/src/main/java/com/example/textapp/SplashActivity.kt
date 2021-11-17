adf1abd7ec8eae4ec509d5362e3f0791a8d12326
DeniZayn <0990759@gmail.com>
2021-11-17 14:13:06 +0300

    HW for Lesson8

---
+++ textapp/src/main/java/com/example/textapp/SplashActivity.kt
@@ -0,0 +1,43 @@
+package com.example.textapp
+
+import android.animation.Animator
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.view.animation.AccelerateDecelerateInterpolator
+import androidx.appcompat.app.AppCompatActivity
+
+
+
+class SplashActivity : AppCompatActivity() {
+    var handler = Handler()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        image_view.animate().rotationBy(650f)
+            .setInterpolator(AccelerateDecelerateInterpolator()).duration = 10000
+            .setListener(object : Animator.AnimatorListener {
+
+                override fun onAnimationEnd(animation: Animator?) {
+                    startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+                    finish()
+                }
+
+                override fun onAnimationRepeat(animation: Animator?) {}
+                override fun onAnimationCancel(animation: Animator?) {}
+                override fun onAnimationStart(animation: Animator?) {}
+            })
+
+                handler.postDelayed({
+            startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+            finish()
+        }, 3000)
+    }
+
+    override fun onDestroy() {
+        handler.removeCallbacksAndMessages(null)
+        super.onDestroy()
+    }
+}
